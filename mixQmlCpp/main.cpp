c6e4ab0ad778d86e72dcca8a3a5d82f107f57137
sunxiaoming <1012021990>
2020-09-22 19:56:57 +0800

    注册自己实现的组件

--- mixQmlCpp/main.cpp
+++ mixQmlCpp/main.cpp
@@ -1,11 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "colormaker.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    qmlRegisterType<ColorMaker>("an.qt.ColorMaker", 1, 0, "ColorMaker");
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
